ce5b34428d32f73243e5bb5c9a37ebcbe64d98b2
Erick Leandro <j.erickleandro@gmail.com>
2026-01-18 18:48:23 -0300

    feat: create schema

--- src/modules/reviews/dto/create-review.dto.ts
+++ src/modules/reviews/dto/create-review.dto.ts
@@ -1,13 +1,53 @@
-import { IsIn, IsNumber, IsString } from "class-validator";
+import { Optional } from "@nestjs/common";
+import { IsArray, IsIn, IsNumber, IsString } from "class-validator";
+
+type Scores = {
+    score: number;
+    userId: string;
+}
 
 export class CreateReviewDto {
     
     @IsString()
     title: string;
 
-    @IsNumber()
-    rating: number;
+    @IsArray()
+    scores: Scores[];
 
     @IsIn(['FILME', 'SERIE', 'GAME', 'LIVRO'])
     type: 'FILME' | 'SERIE' | 'GAME' | 'LIVRO';
+
+    @IsNumber()
+    year: number;
+
+    @IsArray()
+    @IsString({ each: true })
+    genres: string[];
+
+    @IsArray()
+    @IsString({ each: true })
+    platforms: string[];
+
+    @IsString()
+    synopsis: string;
+
+    @Optional()
+    @IsArray()
+    @IsString({ each: true })
+    cast: string[];
+
+    @Optional()
+    @IsString()
+    director?: string;
+
+    @Optional()
+    @IsString()
+    author?: string;
+
+    @Optional()
+    @IsString()
+    publisher?: string;
+
+    @IsString()
+    review: string; 
 }
--- src/modules/reviews/infra/schemas/reviews.schema.ts
+++ src/modules/reviews/infra/schemas/reviews.schema.ts
@@ -1,16 +1,54 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 
-@Schema({ timestamps: true })
-export class Review {
-  @Prop({ required: true })
-  title: string;
+@Schema({ _id: false })
+export class Score {
+  @Prop({ required: true, min: 0 })
+  score: number;
 
   @Prop({ required: true })
-  rating: number;
+  userId: string;
+}
+
+export const ScoreSchema = SchemaFactory.createForClass(Score);
+
+@Schema({ timestamps: true })
+export class Review {
+  @Prop({ required: true, trim: true })
+  title: string;
+
+  @Prop({ type: [ScoreSchema], required: true, default: [] })
+  scores: Score[];
 
   @Prop({ required: true, enum: ['FILME', 'SERIE', 'GAME', 'LIVRO'] })
   type: 'FILME' | 'SERIE' | 'GAME' | 'LIVRO';
+
+  @Prop({ required: true, min: 0 })
+  year: number;
+
+  @Prop({ required: true, type: [String], default: [] })
+  genres: string[];
+
+  @Prop({ required: true, type: [String], default: [] })
+  platforms: string[];
+
+  @Prop({ required: true })
+  synopsis: string;
+
+  @Prop({ type: [String], default: [] })
+  cast?: string[];
+
+  @Prop()
+  director?: string;
+
+  @Prop()
+  author?: string;
+
+  @Prop()
+  publisher?: string;
+
+  @Prop({ required: true })
+  review: string;
 }
 
 export type ReviewDocument = Review & Document;
